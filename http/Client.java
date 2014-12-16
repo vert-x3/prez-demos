9ca5ed68d9396e2fbeb917570689a0469721bc71
Julien Viet <julien@julienviet.com>
2014-12-16 17:02:17 +0100

    http example with rx jackson unmarshalling

--- http/Client.java
+++ http/Client.java
@@ -1,12 +1,20 @@
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.RxHelper;
 import io.vertx.rxjava.core.http.HttpClient;
 import io.vertx.rxjava.core.http.HttpClientRequest;
 import io.vertx.rxjava.core.http.HttpClientResponse;
 
 
 public class Client extends AbstractVerticle {
+
+  public static class Time {
+    public long currentTime;
+    public String toString() {
+      return "Time: " + currentTime;
+    }
+  }
 
   public void start() {
     HttpClient client = vertx.createHttpClient(new HttpClientOptions());
@@ -25,10 +33,10 @@ public class Client extends AbstractVerticle {
         forEach(System.out::println);
     req2.end();
     
-    HttpClientRequest req3 = client.request(HttpMethod.GET, 8080, "localhost", "/data");
+    HttpClientRequest req3 = client.request(HttpMethod.GET, 8080, "localhost", "/time");
     req3.toObservable().
         flatMap(HttpClientResponse::toObservable).
-        map(data -> "Client => " + data.toString("UTF-8")).
+        lift(RxHelper.unmarshaller(Time.class)).
         forEach(System.out::println);
     req3.end();
   }
--- http/Server.java
+++ http/Server.java
@@ -9,11 +9,17 @@ public class Server extends AbstractVerticle {
     server.requestStream().toObservable().forEach( req -> {
       System.out.println("Server => Request: " + req.path());
       try {
-        if (req.path().equals("/error")) {
-          throw new RuntimeException("forced error");
-        }
-        req.response().setChunked(true).end("Path Requested =>: " + req.path() + '\n');
-      } catch(Throwable e) {
+        switch (req.path()) {
+          case "/error":
+            throw new RuntimeException("forced error");
+          case "/time":
+            req.response().putHeader("content-type", "application/json").setChunked(true).end("{\"currentTime\":" + System.currentTimeMillis() + "}");
+            break;
+          default:
+            req.response().setChunked(true).end("Path Requested =>: " + req.path() + '\n');
+            break;
+       }
+    } catch(Throwable e) {
         System.err.println("Server => Error [" + req.path() + "] => " + e);
         req.response().setStatusCode(500).setChunked(true).end("Error 500: Bad Request\n");
       }
